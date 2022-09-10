bd3bddf6e655c4172e301eb7752a98a402b9bf74
iYunghui <f64061224@gs.ncku.edu.tw>
2022-09-10 22:54:46 +0800

    Create home page

--- localLibrary/catalog/urls.py
+++ localLibrary/catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="index"),
+    path('books/', views.books, name="books"),
+    path('authors/', views.author, name="author"),
+    path('book/<int:id>', views.index),
+    path('author/<int:id>', views.index),
 ]
--- localLibrary/catalog/views.py
+++ localLibrary/catalog/views.py
@@ -1,5 +1,34 @@
+from multiprocessing import context
+from urllib import request
 from django.shortcuts import render
+from .models import Genre, Language, Book, BookInstance, Author
 
 # Create your views here.
-def index():
+def index(request):
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+    num_language_type = Language.objects.all().count()
+    num_genre = Genre.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_language_type': num_language_type, 
+        'num_genre': num_genre
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+def author(request):
     render()
+
+
+def books(request):
+    render()
+
