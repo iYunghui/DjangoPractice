3f9bb8dd7d2aa7fb73f8df5c596a7008d8651f16
iYunghui <betty103800@gmail.com>
2022-09-25 22:42:54 +0800

    Create Table

--- museum/face/models.py
+++ museum/face/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
+from pony.orm import *
+
+db = Database()
 
 # Create your models here.
+class Media(db.Entity):
+    uid = Required(str)
+    type = Required(str)
+    media_filename = Required(str)
+    upload_timestamp = Required(datetime.datetime, precision=6)
+
+sql_debug(True)
+db.bind(provider='mysql', host='127.0.0.1', user='root', passwd='password', db='museum_practice')
+db.generate_mapping(create_tables=True)
